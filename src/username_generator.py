2e1cb115fcc39283eeb68c4f29dc6e07943390a8
0b10 <0b10@pm.me>
2019-07-09 16:06:23 +0100

    Create make_get_username

--- src/username_generator.py
+++ src/username_generator.py
@@ -1,9 +1,11 @@
 import requests
 from requests.exceptions import ConnectionError, ConnectTimeout, HTTPError
+from random import randint
 
 import cache
+import config
 
-def config_get_wordlist(do_cache_lookup, timeout=5):
+def make_get_wordlist(do_cache_lookup, timeout=5):
     assert (isinstance(timeout, int) or isinstance(timeout, float)) and timeout > 0, "Timout should be a number > 0"
     assert callable(do_cache_lookup), "The do_cache_lookup param should be a function"
 
@@ -11,22 +13,31 @@ def config_get_wordlist(do_cache_lookup, timeout=5):
         assert url and isinstance(url, str), "The url param should be a non-empty string"
 
         try:
-            return do_cache_lookup(url, lambda: requests.get(url, timeout=timeout).text.split())
+            return do_cache_lookup(url, lambda: requests.get(url, timeout=timeout).text.split()), None
         except ( ConnectTimeout, ConnectionError, HTTPError ) as e:
             return False, e
 
     return get
 
-url = "https://raw.githubusercontent.com/jeanphorn/wordlist/master/usernames.txt"
+def make_get_username(sources, get_wordlist):
+    assert sources and isinstance(sources, list), "The sources param must be a non-empty list"
+    assert callable(get_wordlist), "The get_wordlist param should be a function"
+
+    def _():
+        for source in sources:
+            word_list, error = get_wordlist(source)
+            if not word_list:
+                print(error)
+            else:
+                return word_list[randint(0, len(word_list) - 1)]
+                break
+
+    return _
 
 do_cache_lookup = cache.factory()
-get_wordlist = config_get_wordlist(do_cache_lookup)
-print(get_wordlist(url))
+get_wordlist = make_get_wordlist(do_cache_lookup)
+get_username = make_get_username(config.sources, get_wordlist)
 
-# text = get("https://raw.githubusercontent.com/jeanphorn/wordlist/master/usernames.txt")
-# print(text)
+username = get_username()
 
-
-# populate_cache("baz", [1, 2, 3], setup_cache, make_hash)
-# print(should_cache("baz", make_hash))
-# do_cache("tesddwqiiat22", [1, 2, 3])!
+print(username)
