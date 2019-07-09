f28be601f01f7d24b35301d7d83616e9df3a82a0
0b10 <0b10@pm.me>
2019-07-09 12:54:13 +0100

    Create make_wordlist

--- src/username_generator.py
+++ src/username_generator.py
@@ -1,7 +1,16 @@
 import requests
+from requests.exceptions import ConnectionError, ConnectTimeout, HTTPError
 
-def getDictionary(url):
-    return requests.get(url).text.split()
+def make_wordlist(timeout=5, requests=requests):
+    def get(url):
+        try:
+            return requests.get(url, timeout=timeout).text.split()
+        except ( ConnectTimeout, ConnectionError, HTTPError ) as e:
+            print(e)
+            return False;
+    return get
 
-text = getDictionary("https://raw.githubusercontent.com/jeanphorn/wordlist/master/usernames.txt")
-print(text)
+
+# get = make_wordlist()
+# text = get("https://raw.githubusercontent.com/jeanphorn/wordlist/master/usernames.txt")
+# print(text)
